715ed3c376fea0ca22cccdc9d34d9db0fba56ba5
Per Ulland <perulland@aol.de>
2026-09-04 12:34:23 +0200

    Inhalt vom Design getrennt, echtes Datum, mehr Interaktion

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // lokal auf dem Geraet; das Netz wird nur benutzt, um nachzusehen,
 // ob es etwas Neueres gibt.
 
-const VERSION = "v2";
+const VERSION = "v3";
 const CACHE = "opas-woche-" + VERSION;
 
 const SHELL = [

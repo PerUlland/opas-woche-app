567a2a40068b39ad7628ba623be274ac63d769fc
Per Ulland <perulland@aol.de>
2026-09-04 12:15:50 +0200

    Punkt im Kalender nur bei echten Terminen

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // lokal auf dem Geraet; das Netz wird nur benutzt, um nachzusehen,
 // ob es etwas Neueres gibt.
 
-const VERSION = "v1";
+const VERSION = "v2";
 const CACHE = "opas-woche-" + VERSION;
 
 const SHELL = [
